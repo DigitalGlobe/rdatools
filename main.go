639aafeb3bb33fa93c97425265b89076908a787e
Patrick M Young <patrick.mckendree.young@gmail.com>
2018-08-03 15:57:50 -0600

    Working example in place.
    To fully bake, we need to handle more datatypes and whatnot.

--- main.go
+++ main.go
@@ -30,10 +30,11 @@ type Metadata struct {
 		NumYTiles   int
 		TileXSize   int
 		TileYSize   int
-		MinY        int
 		ImageWidth  int
 		ImageHeight int
 		NumBands    int
+		MinX        int
+		MinY        int
 		MinTileX    int
 		MinTileY    int
 		MaxTileX    int
@@ -52,6 +53,10 @@ type Metadata struct {
 }
 
 func main() {
+	graphID := "be3380f89ac8d4a5eef4d78549f183284d61f0fccbdfed6c17a1c36ac6b38d92"
+	nodeID := "FormatByte"
+
+	// Get GBDX creds.
 	usr, err := user.Current()
 	if err != nil {
 		log.Fatal(err)
@@ -66,10 +71,12 @@ func main() {
 	if !ok {
 		log.Fatalln("no default gbdx credentials found to use")
 	}
+
+	// Get Metadata.
 	client := &http.Client{
 		Timeout: 60 * time.Second,
 	}
-	req, err := http.NewRequest("GET", "https://rda.geobigdata.io/v1/metadata/be3380f89ac8d4a5eef4d78549f183284d61f0fccbdfed6c17a1c36ac6b38d92/FormatByte/metadata.json", nil)
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://rda.geobigdata.io/v1/metadata/%s/%s/metadata.json", graphID, nodeID), nil)
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", conf.Token.AccessToken))
 	res, err := client.Do(req)
 	if err != nil {
@@ -81,14 +88,58 @@ func main() {
 	json.NewDecoder(res.Body).Decode(&md)
 
 	md.ImageMetadata.DataType = "Byte"
-	md.ImageMetadata.NumXTiles = 3
-	md.ImageMetadata.NumYTiles = 3
+	md.ImageMetadata.NumXTiles = 8
+	md.ImageMetadata.NumYTiles = 8
 
-	vrt, err := NewVRT(&md)
+	// Download tiles.
+	tileMap := make(map[string]string)
+	for x := md.ImageMetadata.MinTileX; x < md.ImageMetadata.NumXTiles; x++ {
+		for y := md.ImageMetadata.MinTileY; y < md.ImageMetadata.NumYTiles; y++ {
+			//tileURL := fmt.Sprintf("https://rda.geobigdata.io/v1/tile/%s/%s/%d/%d.tif", graphID, nodeID, x, y)
+			//req, err := http.NewRequest("GET", tileURL, nil)
+			//if err != nil {
+			//	log.Fatal("failed building tile request")
+			//}
+			//req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", conf.Token.AccessToken))
+			//res, err := client.Do(req)
+			//if err != nil {
+			//	log.Fatal("failed requesting tile\n", err)
+			//}
+			//defer res.Body.Close()
+			//
+			//if res.StatusCode != http.StatusOK {
+			//	log.Fatalf("failed to get tile from %s, got code %v %v", tileURL, res.StatusCode, res.Status)
+			//}
+			//
+			//// Write it.
+			fPath := fmt.Sprintf("tmp/tile_%d_%d.tif", x, y)
+			//f, err := os.Create(fPath)
+			//if err != nil {
+			//	log.Fatal("failed opening file on disk")
+			//}
+			//defer f.Close()
+			//
+			//if _, err := io.Copy(f, res.Body); err != nil {
+			//	log.Fatal("failed copying tile to disk")
+			//}
+			//
+			tileMap[fmt.Sprintf("%d/%d", x, y)] = fPath
+		}
+	}
+
+	// Build VRT.
+	vrt, err := NewVRT(&md, tileMap)
 	if err != nil {
 		log.Fatal("failed building vrt")
 	}
-	enc := xml.NewEncoder(os.Stdout)
+
+	f, err := os.Create("rda.vrt")
+	if err != nil {
+		log.Fatal("failure creating VRT file")
+	}
+	defer f.Close()
+
+	enc := xml.NewEncoder(f)
 	enc.Indent("  ", "    ")
 	if err := enc.Encode(vrt); err != nil {
 		fmt.Printf("error: %v\n", err)
--- vrt.go
+++ vrt.go
@@ -6,11 +6,11 @@ import (
 )
 
 type VRTDataset struct {
-	XMLName      xml.Name        `xml:"VRTDataset"`
-	RasterXSize  int             `xml:"RasterXSize,attr"`
-	RasterYSize  int             `xml:"RasterYSize,attr"`
-	SRS          string          `xml:"SRC"`
-	GeoTransform GeoTransform    `xml:"GeoTransform"`
+	XMLName      xml.Name `xml:"VRTDataset"`
+	RasterXSize  int      `xml:",attr"`
+	RasterYSize  int      `xml:",attr"`
+	SRS          string
+	GeoTransform GeoTransform
 	Bands        []VRTRasterBand `xml:"VRTRasterBand"`
 }
 
@@ -19,34 +19,54 @@ type GeoTransform [6]float64
 // <VRTRasterBand dataType="Byte" band="1">
 type VRTRasterBand struct {
 	//XMLName  xml.Name `xml:"VRTRasterBand"`
-	DataType     string         `xml:"dataType,attr"`
-	Band         int            `xml:"band,attr"` // Mask bands would omit empty somehow...
-	SimpleSource []SimpleSource `xml:"SimpleSource"`
+	DataType     string `xml:"dataType,attr"`
+	Band         int    `xml:"band,attr,omitempty"`
+	SimpleSource []SimpleSource
 }
 
-/*      <SimpleSource>
-        <SourceFilename relativeToVRT="1" shared="0">bl/130031330.tif</SourceFilename>
-        <SourceBand>mask,1</SourceBand>
-        <SourceProperties RasterXSize="2387" RasterYSize="2387" DataType="Byte" BlockXSize="256" BlockYSize="256" />
-        <SrcRect xOff="0" yOff="0" xSize="2387" ySize="2387" />
-        <DstRect xOff="0" yOff="4774" xSize="2387" ySize="2387" />
-      </SimpleSource>*/
 type SimpleSource struct {
-	SourceFilename SourceFilename `xml:"SourceFilename"`
-	SourceBand     int            `xml:"SourceBand"`
+	SourceFilename   SourceFilename
+	SourceBand       int
+	SourceProperties SourceProperties
+	SrcRect          Rect
+	DstRect          Rect
+}
+
+type VRTBool bool
+
+func (b VRTBool) MarshalText() (text []byte, err error) {
+	if b {
+		return []byte("1"), nil
+	}
+	return []byte("0"), nil
 }
 
 type SourceFilename struct {
-	RelativeToVRT bool   `xml:"relativeToVRT,attr"`
-	Shared        bool   `xml:"shared,attr"`
-	Filename      string `xml:",chardata"`
+	RelativeToVRT VRTBool `xml:"relativeToVRT,attr"`
+	Shared        VRTBool `xml:"shared,attr"`
+	Filename      string  `xml:",chardata"`
+}
+
+type SourceProperties struct {
+	RasterXSize int    `xml:",attr"`
+	RasterYSize int    `xml:",attr"`
+	DataType    string `xml:",attr"`
+	BlockXSize  int    `xml:",attr"`
+	BlockYSize  int    `xml:",attr"`
+}
+
+type Rect struct {
+	XOff  int `xml:"xOff,attr"`
+	YOff  int `xml:"yOff,attr"`
+	XSize int `xml:"xSize,attr"`
+	YSize int `xml:"ySize,attr"`
 }
 
 func (g GeoTransform) MarshalText() (text []byte, err error) {
 	return []byte(fmt.Sprintf("%.16e, %.16e, %.16e, %.16e, %.16e, %.16e", g[0], g[1], g[2], g[3], g[4], g[5])), nil
 }
 
-func NewVRT(m *Metadata) (*VRTDataset, error) {
+func NewVRT(m *Metadata, tileMap map[string]string) (*VRTDataset, error) {
 
 	gt := [6]float64{
 		m.ImageGeoreferencing.TranslateX,
@@ -58,8 +78,8 @@ func NewVRT(m *Metadata) (*VRTDataset, error) {
 	}
 
 	newGT := gt
-	newGT[0] = gt[0] + gt[1]*float64(m.ImageMetadata.MinTileX*m.ImageMetadata.TileXSize) + gt[2]*float64(m.ImageMetadata.MinTileY*m.ImageMetadata.TileYSize)
-	newGT[3] = gt[3] + gt[4]*float64(m.ImageMetadata.MinTileX*m.ImageMetadata.TileXSize) + gt[5]*float64(m.ImageMetadata.MinTileY*m.ImageMetadata.TileYSize)
+	//newGT[0] = gt[0] + gt[1]*float64(m.ImageMetadata.MinX) + gt[2]*float64(m.ImageMetadata.MinY)
+	//newGT[3] = gt[3] + gt[4]*float64(m.ImageMetadata.MinX) + gt[5]*float64(m.ImageMetadata.MinY)
 
 	vrt := VRTDataset{
 		RasterXSize:  m.ImageMetadata.TileXSize * m.ImageMetadata.NumXTiles,
@@ -77,8 +97,27 @@ func NewVRT(m *Metadata) (*VRTDataset, error) {
 		for x := m.ImageMetadata.MinTileX; x < m.ImageMetadata.NumXTiles; x++ {
 			for y := m.ImageMetadata.MinTileY; y < m.ImageMetadata.NumYTiles; y++ {
 				ss := SimpleSource{
-					SourceFilename: SourceFilename{Filename: fmt.Sprintf("%d_%d.tif", x, y), Shared: false, RelativeToVRT: true},
+					SourceFilename: SourceFilename{Filename: tileMap[fmt.Sprintf("%d/%d", x, y)], Shared: false, RelativeToVRT: true},
 					SourceBand:     b + 1,
+					SourceProperties: SourceProperties{
+						BlockXSize:  m.ImageMetadata.TileXSize,
+						BlockYSize:  m.ImageMetadata.TileYSize,
+						DataType:    m.ImageMetadata.DataType,
+						RasterXSize: m.ImageMetadata.TileXSize,
+						RasterYSize: m.ImageMetadata.TileYSize,
+					},
+					SrcRect: Rect{
+						XOff:  0,
+						YOff:  0,
+						XSize: m.ImageMetadata.TileXSize,
+						YSize: m.ImageMetadata.TileYSize,
+					},
+					DstRect: Rect{
+						XOff:  x * m.ImageMetadata.TileXSize,
+						YOff:  y * m.ImageMetadata.TileYSize,
+						XSize: m.ImageMetadata.TileXSize,
+						YSize: m.ImageMetadata.TileYSize,
+					},
 				}
 				band.SimpleSource = append(band.SimpleSource, ss)
 			}
