3c4d4266dbfaed5525d071331a95ac8aa81d87fa
Patrick Young <patrick.mckendree.young@gmail.com>
2018-10-05 15:25:18 -0600

    Checkin.

---
+++ rda/cmd/configure.go
@@ -0,0 +1,75 @@
+// Copyright © 2018 Patrick Young <patrick.mckendree.young@gmail.com, patrick.young@digitalglobe.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"runtime"
+
+	"github.com/spf13/cobra"
+)
+
+// ensureRDADir will create the RDA directory if it doesn't already exist.
+func ensureRDADir() (string, error) {
+	rdaPath := path.Join(userHomeDir(), ".rda")
+	err := os.MkdirAll(rdaPath, 0600)
+	return rdaPath, err
+}
+
+// userHomeDir returns the home directory of the user in a cross
+// platform way.  I've borrowed this from
+// https://github.com/spf13/viper/blob/master/util.go .
+func userHomeDir() string {
+	if runtime.GOOS == "windows" {
+		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+		return home
+	}
+	return os.Getenv("HOME")
+}
+
+// configureCmd represents the configure command
+var configureCmd = &cobra.Command{
+	Use:   "configure",
+	Short: "Configure RDA access.",
+	// Long: `.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("configure called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(configureCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// configureCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// configureCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
